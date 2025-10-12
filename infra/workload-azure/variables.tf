0583323e10fc5c3fff17e6f71768c5ea415419b6
nobhri <78974799+nobhri@users.noreply.github.com>
2025-10-13 01:34:32 +0900

    fix: linbreak

--- infra/workload-azure/variables.tf
+++ infra/workload-azure/variables.tf
@@ -1,14 +1,38 @@
-variable "prefix"             { type = string  default = "mock" }
-variable "location"           { type = string  default = "japaneast" }
-variable "rg_name"            { type = string  default = "rg-mock-data" }
+variable "prefix"             {
+   type = string  
+   default = "mock" 
+   }
+variable "location"           {
+   type = string  
+   default = "japaneast" 
+   }
+variable "rg_name"            { 
+  type = string  
+  default = "rg-mock-data" 
+  }
 variable "adls_name" { # 3–24 lowercase, globally unique
   type        = string
   description = "ADLS Gen2 account name"
 }
 
-variable "landing_container"  { type = string  default = "landing" }
-variable "uc_root_container"  { type = string  default = "uc-root" }
-variable "bronze_container"   { type = string  default = "bronze" }
-variable "silver_container"   { type = string  default = "silver" }
+variable "landing_container"  { 
+  type = string  
+  default = "landing" 
+  }
+variable "uc_root_container"  { 
+  type = string  
+  default = "uc-root" 
+  }
+variable "bronze_container"   { 
+  type = string  
+  default = "bronze" 
+  }
+variable "silver_container"   { 
+  type = string  
+  default = "silver" 
+  }
 
-variable "workspace_sku"      { type = string  default = "premium" } # or "trial" etc.
+variable "workspace_sku"      { 
+  type = string  
+  default = "premium"
+   } # or "trial" etc.
